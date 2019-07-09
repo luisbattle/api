4ba40251f3437d34d1b1f7d83f9d05e8f251d62f
Luis Batalla <luis.batalla@intraway.com>
2019-07-09 00:24:51 -0300

    find order by Id

--- app.js
+++ app.js
@@ -39,45 +39,34 @@ personRouter
                 console.log(err);
             }
             res.json(data);
-            console.log(query);
         })            
     });
 
-
-// Ruta Order
-var orderRouter = express.Router()
-    .get('/order',function(req,res){
-        var query = req.query;
-        order.find(query, function(err,data){
-            if(data.length==0)
-                console.log('no record found')
-            if(err){
+// Orders
+var orderRouter =  express.Router()
+    .get('/orders' , function(err,res){
+        order.find(function(err,data){
+            if(err)
                 res.status(500).send(err);
-                console.log(err);
-            }
-            res.json(data);
-            console.log(query);
-        });
-    });
-
-    var orderRouter = express.Router()
-    .get('/order/id/:orderId',function(req,res){
-        var query = req.query;
-        order.findById(req.params.orderId, function(err,data){
             if(data.length==0)
-                console.log('no record found')
-            if(err){
-                res.status(500).send(err);
-                console.log(err);
-            }
+                console.log('No record found');
             res.json(data);
-            console.log(query);
-        });
-    });
+        })
+    })
 
+    orderRouter
+    .get('/order/id/:orderId' , function(req,res){
+        order.findById(req.params.orderId,function(err,data){
+            if(err)
+                res.status(500).send(err);
+            if(data.length==0)
+                console.log('No record found');
+            res.json(data);
+            console.log('entro a la orden');
+        })
+    })
 
-
-app.use('/api',personRouter, orderRouter)
+app.use('/api',personRouter, orderRouter);
 
 app.get('/', function(req, res){
     res.send('Welcome to my API');
